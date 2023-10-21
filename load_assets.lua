7938d91b90ed14b5f83d1544846f0f925b5e71f7
kshitijaucharmal <kshitijaucharmal21@gmail.com>
2023-10-21 18:07:01 +0530

    Removed some experimental files

--- load_assets.lua
+++ load_assets.lua
@@ -1,13 +1,9 @@
 
-local ldtk = require('ldtk')
-
 function Assets()
   love.graphics.setDefaultFilter("nearest", "nearest")
 
   local assets = {}
 
-  assets.map = ldtk:load('levels/map1.ldtk')
-  ldtk:goTo(1)
   assets.tileSize = 128
   assets.tileset = love.graphics.newImage("assets/spritesheet_retina.png")
   assets.player = love.graphics.newQuad(assets.tileSize * 9, assets.tileSize * 1, assets.tileSize, assets.tileSize, assets.tileset:getWidth(), assets.tileset:getHeight())
